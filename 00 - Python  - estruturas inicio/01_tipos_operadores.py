ceeb543d2c8b7f8da44158230aafce29f5c0a518
marcogloriavix <marcogloriavix@gmail.com>
2024-05-06 18:45:08 -0300

    Updatet trilha de Python

--- 00 - Python  - estruturas inicio/01_tipos_operadores.py
+++ 00 - Python  - estruturas inicio/01_tipos_operadores.py
@@ -1,4 +1,6 @@
 
+# executar no TERMINAL em Python - clicar na seta |>   - (Run Python File)
+
 # Operadores aritméticos
 #***********************
 produto_1 = 20
@@ -22,4 +24,94 @@ print (saldo == saque)
 print (saldo != saque)
 
 
+# Operadores de Atribuição
+#*************************
+print("*************************")
+print("Operadores de Atribuição")
+saldo  = 500
+print(saldo)
+saldo *= 10 
+print(saldo)
+saldo +=200
+print(saldo)
+saldo -=500
+print(saldo)
+saldo //= 200
+print(saldo)
+saldo /= 10
+print(saldo)
+saldo %= 10
+print(saldo)
+saldo **=10
+print(saldo)
+
+
+
+#....
+
+# Operadores Lógicos
+#*************************
+print("*************************")
+print("Operadores Lógicos")
+
+saldo =1000
+saque=200
+limite = 100
+x=(saldo >= saque and saque <=limite)
+print (x)
+
+x=(saldo >= saque or saque <=limite)
+print (x)
+
+x=not 1000>500
+print(x)
+
+contatos_emergencia=[]
+print(contatos_emergencia)
+
+contatos_emergencia=[]
+print(not contatos_emergencia)
+
+print (True and True)
+print (True and False)
+print (True or True)
+print (True or False)
+
+
+# Operadores de Identidade
+#*************************
+#são operadores utilizados para comparar se os dois objetos
+#testados ocupam a mesma posição na memória
+
+print("*******************************************************")
+print("Operadores de Identidade")
+
+curso = "curso de python"
+nomeCurso  = curso
+saldo,limite = 200,200
+
+x=curso is nomeCurso
+print(x)
+
+x=curso is not nomeCurso
+print(x)
+
+
+# Operadores de Associação
+#*************************
+#são operadores utilizados para verificar se um objeto está presente em uma sequencia
+
+print("*******************************************************")
+print("Operadores de Associação")
+
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500,100]
+
+print("Python" in curso)
+print ("maça" not in frutas)
+print (200 in saques)
+
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
 
---
+++ 00 - Python  - estruturas inicio/02_estruturas.py
@@ -0,0 +1,22 @@
+# executar no TERMINAL em Python - clicar na seta |>   - (Run Python File)
+
+# indentação e blocos
+#***********************
+# no python é MUITO importante INDENTAÇÂO
+print ("**********************************************")
+
+def sacar (valor):
+    saldo =500
+    x=valor
+
+    if saldo >= valor:
+        print("valor sacado!")
+
+sacar(100)
+
+
+
+# Estruturas Condicionais
+#***********************
+# no python é MUITO importante INDENTAÇÂO
+print ("**********************************************")
